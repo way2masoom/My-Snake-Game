f324b45ed7896c78a6ab82c25c769e77bed19dfc
way2masoom <way2masoom@gmail.com>
2024-12-31 12:24:45 +0530

    Added Snake movement Logic

--- script.js
+++ script.js
@@ -10,8 +10,48 @@ document.addEventListener('DOMContentLoaded', function () {
     let gameStarted = false; // game status
 
     let food = { x: 300, y: 200 }; // {X:15*20,y:10*20} // -> call coordinate -> pixels || Pixel of top left Pixel for food
-    let snake = [{ x: 160, y: 200 }, { x: 140, y: 200 }, { x: 120, y: 200 }]; // Position of Snake
+    let snake = [{ x: 160, y: 200 }, { x: 140, y: 200 }, { x: 120, y: 200 }]; // Position of Snake [head,body,body tail]
 
+    let dx = cellSize; // +20;
+    let dy = 0;
+
+    // Update Snake Position based on the direction 
+    function updateSnake() {
+        const newHead = { x: snake[0].x + dx, y: snake[0].y + dy }
+        snake.unshift(newHead); // add new head to the snake
+
+        // checking collision with food
+        if (newHead.x === food.x && newHead.y === food.y) {
+            score += 10;
+            //TODO: Move food
+        } else {
+            snake.pop();
+        }
+    }
+
+    // function to change Direction
+    function changeDirection(e) { // e-> Event
+        console.log("key Press", e);
+        // direction validation for the key press 
+        const isGoingDown = dy === cellSize;
+        const isGoingUp = dy === -cellSize;
+        const isGoingLeft = dx === -cellSize;
+        const isGoingRight = dx == cellSize;
+
+        if (e.key === 'ArrowUp' && !isGoingDown) {
+            dx = 0;
+            dy = -cellSize;
+        } else if (e.key === 'ArrowDown' && !isGoingUp) {
+            dx = 0;
+            dy = cellSize;
+        } else if (e.key === 'ArrowLeft' && !isGoingRight) {
+            dx = -cellSize
+            dy = 0
+        } else if (e.key === 'ArrowRight' && !isGoingLeft) {
+            dx = cellSize
+            dy = 0;
+        }
+    }
 
     // Function to Draw the food Div
     function drawDiv(x, y, className) {
@@ -29,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Making Snake
         snake.forEach((snakeCell) => {
-            const snakeElement = drawDiv(snakeCell.x, snakeCell.y,'snake');
+            const snakeElement = drawDiv(snakeCell.x, snakeCell.y, 'snake');
             gameArena.appendChild(snakeElement);
         })
 
@@ -38,12 +78,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
 
+    // Game loop Function 
+    function gameLoop() {
+        setInterval(() => {
+            updateSnake();
+            drawFoodAndSnake();
+        }, 200)
+    }
+
     // Function to run the Game the user Click on the start button
     function runGame() {
         if (!gameStarted) {
             gameStarted = true;
-            drawFoodAndSnake(); // When game is started draw food & snake
-            // gameLoop();  TODO: Implement game loop
+            // calling the Direction function to move the snake
+            document.addEventListener('keydown', changeDirection)
+
+            // drawFoodAndSnake(); // When game is started draw food & snake
+            gameLoop()
         }
     }
 
