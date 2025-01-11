0c47a579aa0c80341ff50136924ee55b7e18d3d8
Way2masoom <way2masoom@gmail.com>
2025-01-11 10:27:57 +0530

    Game over Function implemented

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const cellSize = 20;
     let score = 0; // score of the game
     let gameStarted = false; // game status
+    let intervalId = '';
 
     let food = { x: 300, y: 200 }; // {X:15*20,y:10*20} // -> call coordinate -> pixels || Pixel of top left Pixel for food
     let snake = [{ x: 160, y: 200 }, { x: 140, y: 200 }, { x: 120, y: 200 }]; // Position of Snake [head,body,body tail]
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // checking collision with food
         if (newHead.x === food.x && newHead.y === food.y) {
             score += 10;
-            //TODO: Move food
+            //TODO: Move food 
         } else {
             snake.pop();
         }
@@ -78,9 +79,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
 
+    // Function to over the function
+    function isGameOver() {
+        // snake Body Collision checks
+        for (let i = 1; i < snake.length; i++) {
+            if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+                return true;
+            }
+        }
+
+        // Wall collision checks
+        const hitLeftWall = snake[0].x < 0 // snake[0]--> head
+        const hitRightWal = snake[0].x > arenaSize - cellSize;
+        const hitTopWall = snake[0].y < 0
+        const hitBottomWall = snake[0].y > arenaSize - cellSize;
+        return hitLeftWall || hitRightWal || hitTopWall || hitBottomWall;
+    }
+
     // Game loop Function 
     function gameLoop() {
-        setInterval(() => {
+        intervalId = setInterval(() => {
+            if (isGameOver()) {
+                clearInterval(intervalId);
+                gameStarted = false;
+                return;
+            }
+
             updateSnake();
             drawFoodAndSnake();
         }, 200)
@@ -93,7 +117,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // calling the Direction function to move the snake
             document.addEventListener('keydown', changeDirection)
 
-            // drawFoodAndSnake(); // When game is started draw food & snake
+            // drawFoodAnd Snake(); // When game is started draw food & snake
             gameLoop()
         }
     }
